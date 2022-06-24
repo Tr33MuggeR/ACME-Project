79eac560c4b7993f36a2858784e5bf4662edb3ac
Joseph Campbell <josephcampbell926@gmail.com>
2022-06-24 13:49:45 -0400

    Return from Eclipse

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -4,5 +4,6 @@ public class Foo	{
 	public static void main(String[] args)	{
 		System.out.println("howdy");
 		System.out.println("hi from GitHub");
+		System.out.println("hi 'joseph1' branch, from Eclipse");
 	}
 }
