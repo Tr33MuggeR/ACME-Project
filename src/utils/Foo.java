93e6cdb5bfeb856ab7a4f7b4f172387e224f707a
Joseph Campbell <josephcampbell926@gmail.com>
2022-06-24 13:56:06 -0400

    Added wit

---
+++ src/utils/Foo.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Foo	{
+	public static void main(String[] args)	{
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'joseph1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("A line of code");
+	}
+}
